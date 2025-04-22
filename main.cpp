657f82af0b8f3c38df98ea42342ef7278c47e63c
murata-kaira <k024g0062@g.neec.ac.jp>
2025-04-22 11:16:36 +0900

    aaa

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include<Windows.h>
 #include<cstdint>
+#include<string>
+#include<format>
 
 //ウィンドウプロシージャ
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg,
@@ -14,17 +16,50 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg,
 		return 0;
 	}
 
-	
-
 	//標準のメッセージ処理を行う
 	return DefWindowProc(hwnd, msg, wparam, lparam);
 }
 
 
+void Log(const std::string& message) {
+	OutputDebugStringA(message.c_str());
+}
 
+std::wstring ConvertString(const std::string& str) {
+	if (str.empty()) {
+		return std::wstring();
+	}
+	auto sizeNeeded = MultiByteToWideChar(CP_UTF8, 0,
+		reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()),
+		NULL, 0);
+	if (sizeNeeded == 0) {
+		return std::wstring();
+	}
+	std::wstring result(sizeNeeded, 0);
+	MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]),
+		static_cast<int>(str.size()), &result[0], sizeNeeded);
+	return result;
+}
+
+std::string ConvertString(const std::wstring& str) {
+	if (str.empty()) {
+		return std::string();
+	}
+	auto sizeNeeded = WideCharToMultiByte(CP_UTF8, 0,
+		str.data(), static_cast<int>(str.size()), NULL, 0, NULL, NULL);
+	if (sizeNeeded == 0) {
+		return std::string();
+	}
+	std::string result(sizeNeeded, 0);
+	WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()),
+		result.data(), sizeNeeded, NULL, NULL);
+	return result;
+}
 
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+
+	
 
 WNDCLASS wc{};
 	//ウィンドウプロシージャ
@@ -91,6 +126,9 @@ WNDCLASS wc{};
 
 
 	OutputDebugStringA("Hello,DirectX!\n");
+
+	Log(ConvertString(std::format(L"WSTRING{}\n", L"abc")));
+
 
 	return 0;
 
